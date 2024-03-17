faa9a9bee8bbc84f1b2fa76972cd16476e1d99a5
Fatima Tariq <133150135+FatimaTariq07@users.noreply.github.com>
2024-03-17 18:01:50 +0500

    Add files via upload

---
+++ Fictorial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    class Fictorial
+    {
+        private int number;
+        
+
+        public void Input()
+        {
+            Console.Write("Enter the number \t");
+            number = int.Parse(Console.ReadLine());
+        }
+        public int Fact()
+        {
+            int result=1;
+            for (int i = 0; i < number;i++ )
+            {
+                result = result * number;
+                number--;
+            }
+            return result;
+        }
+
+        public void Display(int res)
+        {
+            Console.WriteLine("The number is {0}", number);
+            Console.WriteLine("The fictorial of {0} is {1}", number, res);
+        }
+    }
+}
